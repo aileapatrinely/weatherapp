e4af6b3e3aa4941e0b249aa9ad29c18589b0859f
Ailea Patrinely <aileapatrinely@gmail.com>
2020-12-02 13:40:44 -0600

    sets up index.js

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {createStore, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import createSagaMiddleware from 'redux-saga';
+import logger from 'redux-logger';
+
+import App from './components/App/App';
+
+const sagaMiddleware = createSagaMiddleware();
+
+const middlewareList = process.env.NODE_ENV === 'development' ?
+  [sagaMiddleware, logger] :
+  [sagaMiddleware];
+
+const store = createStore(
+    //will need to put root reducer in here.
+    applyMiddleware(),
+);
+
+//sagaMiddleware.run(rootSaga);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>
+    document.getElementById('react-root'),
+);
