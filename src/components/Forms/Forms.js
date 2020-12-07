aabd334b9f71822fa83144ec0c10f68982971ba2
Ailea Patrinely <aileapatrinely@gmail.com>
2020-12-07 10:29:01 -0600

    creates form for submitting location

---
+++ src/components/Forms/Forms.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import mapStoreToProps from '../../redux/mapStoreToProps';
+import { withRouter } from 'react-router-dom';
+
+class Forms extends Component {
+    state = {
+        city:'',
+        state:'',
+    }
+
+    handleInputChangeFor = (propertyName) => (event) => {
+        this.setState({
+          [propertyName]: event.target.value,
+        });
+      };
+
+    render(){
+        return(
+            <div>
+                <form>
+                    <input type="text" name="City" value={this.state.city} required onChange={this.handleInputChangeFor('City')}/>
+                    <input type="text" name="State" value={this.state.state} required onChange={this.handleInputChangeFor('State')}/>
+                    <input type="submit" name="Submit"/>
+                </form>
+            </div>
+        )
+    }
+}
